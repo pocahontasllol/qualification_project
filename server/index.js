3612bbbe402d8be7ef242ea5e742a2040ae6d0d3
pocahontasllol <pocahontasllol@gmail.com>
2022-12-20 18:41:44 +0300

    server folder

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import multer from "multer";
 import mongoose from "mongoose";
-
+import cors from "cors"
 import {
   registerValidation,
   loginValidations,
@@ -37,6 +37,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors())
 app.use("/uploads", express.static("uploads"));
 
 app.post(
